22b9b10fcb16093432d75373114aabe46e3d06e1
@ <@>
2021-07-31 14:42:23 +0800

    update

--- build_windows.bat
+++ build_windows.bat
@@ -1,3 +1,9 @@
+git pull
 go get -u
-go build -ldflags="-s -w" -tags=tempdll
+go get github.com/a97077088/libfridabinres@master
+go build -ldflags="-s -w" -tags=tempdll -o fd.exe
 upx.exe fd.exe
+git add go.sum
+git add go.mod
+git commit -m "build_windows"
+git push
