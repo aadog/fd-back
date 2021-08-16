600ef329043e3f4fc75bc9316863a0a919386d25
@ <@>
2021-08-16 12:55:19 +0800

    update build_linux.sh and build_macos.sh

--- build_linux.sh
+++ build_linux.sh
@@ -3,7 +3,3 @@ go get -u
 go get github.com/a97077088/libfridabinres@master
 go build -ldflags="-s -w" -tags=tempdll -o fd_linux
 upx ./fd_linux
-git add go.sum
-git add go.mod
-git commit -m "fd_linux"
-git push
--- build_macos.sh
+++ build_macos.sh
@@ -3,7 +3,3 @@ go get -u
 go get github.com/a97077088/libfridabinres@master
 go build -ldflags="-s -w" -tags=tempdll -o fd_macos
 upx ./fd_macos
-git add go.sum
-git add go.mod
-git commit -m "build_macos"
-git push
